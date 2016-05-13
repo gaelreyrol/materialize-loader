47fab00ac219ae234fbf4f5b622a8aa03390212e
Gaël Reyrol <gael.reyrol@makeidea.io>
2016-05-13 22:49:46 +0200

    Improved error message on load

--- index.loader.js
+++ index.loader.js
@@ -8,17 +8,14 @@ module.exports.pitch = function (remainingRequest) {
   this.cacheable(true);
   if (!configFilePath || configFilePath.trim() === '') {
     var msg = 'You specified the materialize-loader with no configuration file. Please specify' +
-      ' the configuration file, like: \'materialize-loader!./materialize.config.js\'';
+      ' the configuration file, like: \'materialize-loader!./materialize.config.js\' or use' +
+      ' require(\'materialize-loader\').';
     console.error('ERROR: ' + msg);
     throw new Error(msg);
   }
 
   var config = require(configFilePath);
   var styleLoader = config.styleLoader || 'style-loader!css-loader!sass-loader';
-  if (process.env.NODE_ENV === 'production') {
-    var ExtractTextPlugin = require('extract-text-webpack-plugin');
-    styleLoader = ExtractTextPlugin.extract('style', 'css!sass');
-  }
 
   var styleLoaderCommand = 'require(' + JSON.stringify('-!' + styleLoader + '!' +
       require.resolve('./materialize-styles.loader.js') + '!' + configFilePath) + ');';
