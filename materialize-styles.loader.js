d7315f6cdb9ce3885b85fb2777b316f8128b18ff
Gaël Reyrol <gael@soyhuce.fr>
2017-07-13 10:55:33 +0200

    fix(loader): Adjust materialize-css font path

--- materialize-styles.loader.js
+++ materialize-styles.loader.js
@@ -1,19 +1,19 @@
 var styles = ["materialize"];
 
-module.exports = function(content) {
+module.exports = function (content) {
   this.cacheable(true);
   var config = this.exec(content, this.resourcePath);
   var start =
-    '$roboto-font-path: "~materialize-css/fonts/roboto/";\n' +
+    '$roboto-font-path: "~materialize-css/dist/fonts/roboto/";\n' +
     '@import "~materialize-css/sass/components/_color.scss";\n' +
     '@import "./materialize.config.scss";\n';
   source =
     start +
     styles
-      .filter(function(style) {
+      .filter(function (style) {
         return config.styles[style];
       })
-      .map(function(style) {
+      .map(function (style) {
         return '@import "~materialize-css/sass/' + style + '.scss";';
       })
       .join("\n");
